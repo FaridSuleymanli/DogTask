6f5281a1c3d15dd741e09e25f99ece78bc8d801c
Farid Suleymanli <faridms@code.edu.az>
2022-02-09 13:05:12 +0400

    Created controller and DTO

---
+++ API/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TestTask.DAL;
+
+namespace TestTask.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        private readonly TestTaskContext _context;
+        private readonly IMapper _mapper;
+        public HomeController(TestTaskContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+        [HttpGet]
+        [Route("Ping")]
+        public ActionResult Ping()
+        {
+            return Ok("Dogs house service. Version 1.0.1");
+        }
+
+        [HttpGet]
+        [Route("Dogs")]
+        public async Task<ActionResult> GetAllDogs()
+        {
+
+        }
+    }
+}
---
+++ Entities/DTOs/DogsForGetDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestTask.Entities.DTOs
+{
+    public class DogsForGetDTO
+    {
+        public string Name { get; set; }
+        public string Color { get; set; }
+        public int Tail_Length { get; set; }
+        public int Weight { get; set; }
+    }
+}
