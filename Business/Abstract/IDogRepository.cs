f462913c0d1d3238a7b99989e98179f15ab9c0ae
Farid Suleymanli <faridms@code.edu.az>
2022-02-10 20:16:18 +0400

    Created business layer

---
+++ Business/Abstract/IDogRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TestTask.Entities.Models;
+
+namespace TestTask.Business.Abstract
+{
+    public interface IDogRepository
+    {
+        List<Dog> GetAllDogs(string attribute, string order);
+    }
+}
---
+++ Business/Concrete/DogRepository.cs
@@ -0,0 +1,69 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TestTask.Business.Abstract;
+using TestTask.DAL;
+using TestTask.Entities.DTOs;
+using TestTask.Entities.Models;
+
+namespace TestTask.Business.Concrete
+{
+    public class DogRepository : IDogRepository
+    {
+        private readonly TestTaskContext _context;
+        private readonly IMapper _mapper;
+        //public static int PAGE_SIZE { get; set; } = 5;
+        public DogRepository(TestTaskContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+        public List<Dog> GetAllDogs(string attribute, string order)
+        {
+            IEnumerable<DogsForGetDTO> dogs = _mapper.Map<IEnumerable<DogsForGetDTO>>(_context.Dogs.AsQueryable());
+            #region Sorting
+            dogs = dogs.OrderBy(d => d.Name);
+            if (!string.IsNullOrEmpty(attribute) && !string.IsNullOrEmpty(order))
+            {
+                if (attribute == "color")
+                {
+                    switch (order)
+                    {
+                        case "desc": dogs = dogs.OrderByDescending(d => d.Color);
+                            break;
+                        case "asc": dogs = dogs.OrderBy(d => d.Color);
+                            break;
+                    }
+                }
+                else if (attribute == "tail_length")
+                {
+                    switch (order)
+                    {
+                        case "desc":
+                            dogs = dogs.OrderByDescending(d => d.Tail_Length);
+                            break;
+                        case "asc":
+                            dogs = dogs.OrderBy(d => d.Tail_Length);
+                            break;
+                    }
+                }
+                else if (attribute == "weight")
+                {
+                    switch (order)
+                    {
+                        case "desc":
+                            dogs = dogs.OrderByDescending(d => d.Weight);
+                            break;
+                        case "asc":
+                            dogs = dogs.OrderBy(d => d.Weight);
+                            break;
+                    }
+                }
+            }
+            #endregion
+            throw new NotImplementedException();
+        }
+    }
+}
