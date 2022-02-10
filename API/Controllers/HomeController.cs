e58047083640f8496db1421727aece690b8da78a
Farid Suleymanli <faridms@code.edu.az>
2022-02-10 17:44:46 +0400

    Added get list

--- API/Controllers/HomeController.cs
+++ API/Controllers/HomeController.cs
@@ -1,11 +1,14 @@
 ﻿using AutoMapper;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using TestTask.DAL;
+using TestTask.Entities.DTOs;
+using TestTask.Entities.Models;
 
 namespace TestTask.API.Controllers
 {
@@ -31,7 +34,28 @@ namespace TestTask.API.Controllers
         [Route("Dogs")]
         public async Task<ActionResult> GetAllDogs()
         {
-
+            IEnumerable<DogsForGetDTO> dogs = _mapper.Map<IEnumerable<DogsForGetDTO>>(await _context.Dogs.ToListAsync());
+            if (dogs == null)
+            {
+                return NotFound(new { message = "Dog list is empty" });
+            }
+            return Ok(dogs);
         }
+
+        [HttpGet]
+        [Route("Dogs/{id}")]
+        public async Task<ActionResult> GetDogById(int id)
+        {
+            var dog = _mapper.Map<Dog, DogsForGetDTO>(await _context.Dogs.FirstOrDefaultAsync(d => d.Id == id));
+            if (dog == null)
+            {
+                return BadRequest(new { error = "Object with the given {id} was not found"});
+            }
+            return Ok(dog);
+        }
+
+        [HttpPost]
+        [Route("Dog")]
+        public async Task<ActionResult> CreateDog([FromBody])
     }
 }
